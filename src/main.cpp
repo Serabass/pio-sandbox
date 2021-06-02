e09ab3afc0cb7b31ad5f6ec0c36f22b0cdb923da
Serabass <serabass565@gmail.com>
2021-06-02 20:01:58 +0600

    upd

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "Arduino.h"
 #include "test.h"
 
-// #define DEBUG
+#define DEBUG
 
 #ifdef DEBUG
 #include "avr8-stub.h"
@@ -27,11 +27,11 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
-  delay(1000);
+  delay(300);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
   // wait for a second
-  delay(1000);
+  delay(300);
 
   a = test->add(a, 1);
 }
